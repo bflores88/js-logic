3aee2bf347a325d94a7abd9002f139b1dc3f9e9f
kmietotwy <kmietotwy@gmail.com>
2019-01-12 03:54:24 -1000

    added dailySpecials function

--- exercises.js
+++ exercises.js
@@ -354,7 +354,37 @@ console.log(doughnutBought);
 /*Create a function name dailySpecials which takes in a parameter: `special`.
 Inside the function, create a switch statement that will check the daily specials of your favorite restaurant (or make up your own daily specials for each day of the week.*/
 
+  var dailySpecials = 'Monday';
+  var special = null;
+  
+  switch (dailySpecials) {
+    case 'Monday':
+    special = 'spicy ahi don';
+      break;
+    case 'Tuesday':
+    special = 'misoyaki butterfish';
+      break;
+    case 'Wednesday' :
+    special = 'gyudon';
+      break;
+    case 'Thursday' :
+    special = 'okonomiyaki';
+      break;
+    case 'Friday' :
+    special = 'tonkotsu ramen';
+      break;
+    case 'Saturday' :
+    special = 'beef sukiyaki';
+      break;
+    default: 
+    special = 'takoyaki';
+  
+  }
 
+  
+  console.log(special); 
+  
+  
 
 /*
 For loops - A for loop checks a condition a specific number of times and allows us to execute a code block and evaluate a condition to determine if our loop should run again.
